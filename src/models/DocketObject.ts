b2c5abad085c74340fb0fff6d5c3bcf0b134280c
jamesjd210 <jamesjd1024@gmail.com>
2023-10-18 23:15:03 -0500

    updated db spec to use companyName as key instead of user

--- src/models/DocketObject.ts
+++ src/models/DocketObject.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb";
 
 export default class DocketObject {
-    constructor(public user : string, public currApiForm: Object, public currApiRequest: Object, public codeTranslations: Object, public response : Object,  public id?: ObjectId) {}
+    constructor(public companyName : string, public currApiForm: Object, public currApiRequest: Object, public codeTranslations: Object, public response : Object,  public id?: ObjectId) {}
 }
